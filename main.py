9bd7d2f051379ce58f72eeafced5e82d7d6b9e9b
ksivabalan <ksivabalan.91@gmail.com>
2024-10-19 12:15:50 +0100

    Find all scenes, Create title, Create Set with download link, done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import re
 import time
 from dotenv import load_dotenv
 from selenium import webdriver
@@ -6,6 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# pt-[10px] pr-[15px] mb-5 h-[20px] text-zinc-300 text-xl leading-none
 
 load_dotenv(override=True)
 LOGIN_URL = os.getenv("LOGIN_URL")
@@ -13,7 +15,11 @@ MEMBER_URL = os.getenv("MEMBER_URL")
 MAX_ATTEMPTS = 3
 driver = webdriver.Chrome()
 
+allScenes = []
+linkSet = set()
+
 def main():
+    #! LOGIN
     driver.get(MEMBER_URL)
     for i in range(1, MAX_ATTEMPTS +1):
         if("mem" not in driver.current_url):
@@ -22,7 +28,21 @@ def main():
         else:
             break
     print("Successfully logged in!")
-    
+
+    #! GET ALL SCENES
+    findAllScenes()
+    for i in range(2,3):
+        driver.get(f"{MEMBER_URL}/?page={i}")
+        WebDriverWait(driver,10).until(EC.url_changes)
+        findAllScenes()
+    print(f"{len(allScenes)} scenes found")
+
+    #! GET ALL DOWNLOAD LINKS
+    for scene in allScenes:
+        driver.get(scene)
+        WebDriverWait(driver,10).until(EC.url_changes)
+        buildLinkLibrary()
+    print(f"Link library built, {len(linkSet)} links extracted")
     time.sleep(15)
     driver.close()
     return
@@ -47,10 +67,39 @@ def login():
         input_password = driver.find_element(By.ID, "password")
         input_password.send_keys(os.getenv("PASSWORD"))
         driver.find_element(By.NAME,"commit").click()
-    except:
+    except Exception as e:
         print(f"Unable to login: {e}")
 
-    return   
+    return
+
+def findAllScenes():
+    scene_links = driver.find_elements(By.XPATH, "//a[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),'video clip')]")
+    for elem in scene_links:
+        link = elem.get_attribute("href")
+        allScenes.append(link)
+    return
+
+def buildLinkLibrary():
+    main_content = driver.find_element(By.ID, "main_content")
+    sub_content = main_content.find_elements(By.TAG_NAME,"div")
+    
+    title = sub_content[0].find_element(By.TAG_NAME,"a").text
+    title = re.sub(r',?\s*page.*$', '', title).strip()
+
+    for div in sub_content:
+        try:
+            video_caption_elem = div.find_element(By.CLASS_NAME, "video_caption")
+            subtitle = f"{title} -- {video_caption_elem.find_element(By.TAG_NAME, "b").text.strip()}"
+            download_elem = div.find_element(By.CLASS_NAME,"media_download_block")
+            link_elem = download_elem.find_element(By.XPATH,".//a[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),'mp4')]")
+            download_link = link_elem.get_attribute('href')
+            print(f"{subtitle} >> {download_link}")
+            linkSet.add((subtitle,download_link))
+        except Exception as e:
+            # print ({e})
+            # Handle the case where video_caption is not found
+            continue        
+    return
 
 if __name__ ==  "__main__":
     main()
