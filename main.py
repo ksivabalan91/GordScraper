6a07ebaea9136fb3a59b1704130c52374147000e
ksivabalan <ksivabalan.91@gmail.com>
2024-10-21 10:56:27 +0100

    Add type annotations

--- main.py
+++ main.py
@@ -4,33 +4,30 @@ from login import login
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.remote.webelement import WebElement
 
 # Load environment variables
 load_dotenv(override=True)
-MEMBER_URL = os.getenv("MEMBER_URL")
+MEMBER_URL: str | None = os.getenv("MEMBER_URL")
 driver = webdriver.Chrome()
 
-def main():
-    allScenes = []
-    linkSet = set()
+def main() -> None:
+    allScenes: list[str] = []
+    linkSet: set = set()
     
     #! LOGIN
     login(driver)
 
     #! GET ALL SCENES
-    allScenes = findAllScenes(allScenes)
-    for i in range(2,65):
+    for i in range(1,65):
         driver.get(f"{MEMBER_URL}/?page={i}")
-        WebDriverWait(driver,10).until(EC.url_changes)
+        print(f"Page {i}")
         allScenes = findAllScenes(allScenes)
     print(f"{len(allScenes)} scenes found")
 
     #! GET ALL DOWNLOAD LINKS
     for index, scene in enumerate(allScenes):
         driver.get(scene)
-        WebDriverWait(driver,10).until(EC.url_changes)
         linkSet = buildLinkLibrary(linkSet)
         print(f"{index}/{len(allScenes)}")
     print(f"Link library built, {len(linkSet)} links extracted")    
@@ -42,23 +39,23 @@ def main():
     driver.close()
     return    
 
-def findAllScenes(allScenes):
+def findAllScenes(allScenes: list[str]) -> list[str]:
     try:
         # Look for "video clip" text on page and find element
-        scene_links = driver.find_elements(By.XPATH, "//a[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),'video clip')]")
+        scene_links: list[WebElement] = driver.find_elements(By.XPATH, "//a[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),'video clip')]") # type: ignore
         for elem in scene_links:
             # Get the page link
-            link = elem.get_attribute("href")
-            allScenes.append(link)
+            link: str | None = elem.get_attribute("href")
+            if link: allScenes.append(link)
     except Exception as e:
         print(e)
     return allScenes
 
-def buildLinkLibrary(linkSet):
-    title =""
+def buildLinkLibrary(linkSet: set) -> set:
+    title: str =""
     try:
-        main_content = driver.find_element(By.ID, "main_content")
-        sub_content = main_content.find_elements(By.TAG_NAME,"div")
+        main_content: WebElement = driver.find_element(By.ID, "main_content")
+        sub_content: list[WebElement] = main_content.find_elements(By.TAG_NAME,"div")
         # In the first div element, find a tag and get the text
         title = sub_content[0].find_element(By.TAG_NAME,"a").text
         # Regex to drop 'page' text if any
@@ -69,21 +66,21 @@ def buildLinkLibrary(linkSet):
     for div in sub_content:
         try:
             # Find subtitle element
-            video_caption_elem = div.find_element(By.CLASS_NAME, "video_caption")
+            video_caption_elem: WebElement = div.find_element(By.CLASS_NAME, "video_caption")
             # Add subtitle to main title for filename
-            subtitle = f"{title} -- {video_caption_elem.find_element(By.TAG_NAME, "b").text.strip()}"
+            subtitle: str = f"{title} -- {video_caption_elem.find_element(By.TAG_NAME, "b").text.strip()}"
             # Locate download link block
-            download_elem = div.find_element(By.CLASS_NAME,"media_download_block")
+            download_elem: WebElement = div.find_element(By.CLASS_NAME,"media_download_block")
             # Only find elements with mp4 in its text 
-            link_elem = download_elem.find_element(By.XPATH,".//a[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),'mp4')]")
-            download_link = link_elem.get_attribute('href')
+            link_elem: WebElement = download_elem.find_element(By.XPATH,".//a[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),'mp4')]")
+            download_link: str | None = link_elem.get_attribute('href')
             print(f"{subtitle} >> {download_link}")
             linkSet.add((subtitle,download_link))
         except Exception as e:
             continue        
     return linkSet
 
-def exportData(filename, linkSet, columns):
+def exportData(filename, linkSet, columns) -> None:
     try:
         # Create a DataFrame from the linkSet
         df = pd.DataFrame(linkSet, columns=columns)        
